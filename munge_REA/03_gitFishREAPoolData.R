dbfe7600166993471dc36e3f3eef37de31be9be8
Ivor Williams <ivor.williams@noaa.gov>
2017-10-05 09:12:23 -1000

    minor fix

--- munge_REA/03_gitFishREAPoolData.R
+++ munge_REA/03_gitFishREAPoolData.R
@@ -110,17 +110,17 @@ dp<-Calc_Pooled_Simple(dps$Mean, dps$SampleVar, data.cols, OUTPUT_LEVEL, "AREA_H
 save(dp, file="data_pooled_is_yr_RZ.Rdata")
 
 # e.g. SAVE BY ISLAND PER YEAR
-AGGREGATION_LEVEL<-c("REGION","ISLAND", "ANALYSIS_YEAR")
+OUTPUT_LEVEL<-c("REGION","ISLAND", "ANALYSIS_YEAR")
 dp<-Calc_Pooled_Simple(dps$Mean, dps$SampleVar, data.cols, OUTPUT_LEVEL, "AREA_HA")
 save(dp, file="data_pooled_is_yr.Rdata")
 
 # e.g. SAVE BY ISLAND POOLING ALL YEARS' DATA
-AGGREGATION_LEVEL<-c("REGION","ISLAND")
+OUTPUT_LEVEL<-c("REGION","ISLAND")
 dp<-Calc_Pooled_Simple(dps$Mean, dps$SampleVar, data.cols, OUTPUT_LEVEL, "AREA_HA")
 save(dp, file="data_pooled_is.Rdata")
 
 # e.g. SAVE BY ISLAND AND SECTOR PER YEAR
-AGGREGATION_LEVEL<-c("REGION","ISLAND", "ANALYSIS_SEC", "ANALYSIS_YEAR")
+OUTPUT_LEVEL<-c("REGION","ISLAND", "ANALYSIS_SEC", "ANALYSIS_YEAR")
 dp<-Calc_Pooled_Simple(dps$Mean, dps$SampleVar, data.cols, OUTPUT_LEVEL, "AREA_HA")
 save(dp, file="data_pooled_SEC_yr.Rdata")
 
