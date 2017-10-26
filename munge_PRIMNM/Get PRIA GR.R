9220700e267b0f9a9ebfec72cee69e1e0d9e2e3c
Ivor Williams <ivor.williams@noaa.gov>
2017-10-26 09:35:10 -1000

    gis code

---
+++ munge_PRIMNM/Get PRIA GR.R
@@ -0,0 +1,78 @@
+setwd("/Users/ivor.williams/Documents/CRED/Fish Team/PRIA Report")
+rm(list=ls())
+library(gdata)             # needed for drop_levels()
+library(reshape)           # reshape library inclues the cast() function used below
+
+library(sp)
+library(rgdal) 
+library(rgeos)
+library(maptools)
+
+source("/Users/ivor.williams/Documents/CRED/Fish Team/Git/fish-paste/lib/fish_team_functions.R")
+
+get_GEOREGION<-function(x, shp){ 
+	# function assumes that x is a data frame with at least some identifier column (eg SITEVISITID), LATITUDE, LONGITUDE
+	# shp is a Shapefile, LOCATION tells function how to deal with differences among regions (eg that Guam needs two calls)
+
+	proj<-proj4string(shp)
+	wgs.84 <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0" ## projection for field collected data
+
+	datr<-x
+	coordinates(datr)<- ~ LONGITUDE + LATITUDE
+	#class(datr); str(datr); coordinates(datr); range(datr@coords[,2]); range(datr@coords[,1])
+	
+	## assign dataframe as unprojected data points as the coordinate reference system
+	proj4string(datr)
+	proj4string(datr) <- CRS(wgs.84)
+	range(datr@coords[,2]); range(datr@coords[,1])
+	datr<- spTransform(datr, proj) ### transform projection to match the shape file proj
+	
+	tmp<-over(datr, shp)
+	x$GEOREGION<-tmp$GEOREGION
+	
+	return(x)		
+} # end get_GEOREGION
+
+
+## LOAD AND CLEAN fish data
+load("/Users/ivor.williams/Documents/CRED/Fish Team/Git/fish-paste/data/ALL_REA_FISH_RAW.rdata")
+x<-df
+
+# HOUSEKEEPING ------------------------------------------------------------
+# clean up the data to only fields we currently use
+DATA_COLS<-c("SITEVISITID", "METHOD", "REGION", "ISLAND", "OBS_YEAR", "SITE", "LATITUDE", "LONGITUDE")
+head(x[,DATA_COLS])
+x<-x[,DATA_COLS]
+
+## Update SITE to have five numeric digits (eg OAH-01 becomes OAH-00001)
+x$SITE<-SiteNumLeadingZeros(x$SITE)
+x<-subset(x, x$REGION == "PRIAs") 
+wd<-droplevels(x)
+
+#get base survey info, calculate average depth+complexity+so on
+wd$COUNT<-0  #just need for next function
+st<-Aggregate_InputTable(wd, DATA_COLS)
+
+ISLANDS<-unique(wd$ISLAND)
+
+w<-get_GEOREGION(st[st$ISLAND=="Wake",], readOGR("/Users/ivor.williams/Documents/CRED/Fish Team/PRIA Report/Georegions/WAK_Georegion.shp"))
+summary(w)
+j<-get_GEOREGION(st[st$ISLAND=="Jarvis",], readOGR("/Users/ivor.williams/Documents/CRED/Fish Team/PRIA Report/Georegions/JAR_Georegion.shp"))
+summary(j)
+k<-get_GEOREGION(st[st$ISLAND=="Kingman",], readOGR("/Users/ivor.williams/Documents/CRED/Fish Team/PRIA Report/Georegions/KIN_Georegion.shp"))
+summary(k)
+b<-get_GEOREGION(st[st$ISLAND=="Baker",], readOGR("/Users/ivor.williams/Documents/CRED/Fish Team/PRIA Report/Georegions/BAK_Georegion.shp"))
+summary(b)
+h<-get_GEOREGION(st[st$ISLAND=="Howland",], readOGR("/Users/ivor.williams/Documents/CRED/Fish Team/PRIA Report/Georegions/HOW_Georegion.shp"))
+summary(h)
+p<-get_GEOREGION(st[st$ISLAND=="Palmyra",], readOGR("/Users/ivor.williams/Documents/CRED/Fish Team/PRIA Report/Georegions/PAL_Georegion.shp"))
+summary(p)
+jo<-get_GEOREGION(st[st$ISLAND=="Johnston",], readOGR("/Users/ivor.williams/Documents/CRED/Fish Team/PRIA Report/Georegions/JOH_Georegion.shp"))
+summary(jo)
+
+all.geo<-rbind(w, j, k, b, h, p, jo)
+summary(all.geo)
+
+save(all.geo, file="PRIA Site Georegions.RData")
+
+
---
+++ munge_PRIMNM/Get PRIA TowRZ.R
@@ -0,0 +1,73 @@
+setwd("/Users/ivor.williams/Documents/CRED/Fish Team/PRIA Report")
+rm(list=ls())
+library(gdata)             # needed for drop_levels()
+library(reshape)           # reshape library inclues the cast() function used below
+
+library(sp)
+library(rgdal) 
+library(rgeos)
+library(maptools)
+
+st<-read.csv("tmpPRIAtow.csv")    		#I created this already .. its a list of all the segments  
+st$LONGITUDE<-st$CENTROIDLON			#creating LATITUDE and LONGITUDE fields .. as that is what the code below expects
+st$LATITUDE<-st$CENTROIDLAT
+head(st)
+st$x<-NULL
+
+source("/Users/ivor.williams/Documents/CRED/Fish Team/Git/fish-paste/lib/fish_team_functions.R")
+
+#This fuction pulls REEF_ZONE from the shapefile for the sites ... 
+# this code as written assumes that the shapefile is in wgs.84 and that it has  value REEF_ZONE that can be assocaited with a lat/long
+# REEF_ZONE will be attached to the input data.frame x and returned.
+get_RZ<-function(x, shp){ 
+#x is a data.frame with each record being a location of interest (eg sruvey site or tow segment) and including fields LATITUDE & LONGITUDE
+#shp is a shapefile structure created using a function call like ... readOGR("./ReefZones/kin_reefzones.shp")
+#requires these libraries
+# library(sp)
+# library(rgdal) 
+# library(rgeos)
+# library(maptools)
+
+
+	# function assumes that x is a data frame with at least some identifier column (eg SITEVISITID), LATITUDE, LONGITUDE
+	# shp is a Shapefile
+
+	proj<-proj4string(shp)
+	wgs.84 <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0" ## projection for field collected data
+
+	datr<-x
+	coordinates(datr)<- ~ LONGITUDE + LATITUDE
+	#class(datr); str(datr); coordinates(datr); range(datr@coords[,2]); range(datr@coords[,1])
+	
+	## assign dataframe as unprojected data points as the coordinate reference system
+	proj4string(datr)
+	proj4string(datr) <- CRS(wgs.84)
+	range(datr@coords[,2]); range(datr@coords[,1])
+	datr<- spTransform(datr, proj) ### transform projection to match the shape file proj
+	
+	tmp<-over(datr, shp)
+	x$REEF_ZONE<-tmp$REEF_ZONE
+	
+	return(x)		
+} # end get_RZ
+
+
+k<-get_RZ(st[st$ISLAND=="Kingman",], readOGR("./ReefZones/kin_reefzones.shp"))   #I have subdirectory "ReefZones" with all the shapefile information
+head(k)    #same as st .. but with REEF_ZONE added at the end
+j<-get_RZ(st[st$ISLAND=="Johnston",], readOGR("./ReefZones/joh_reefzones.shp"))   #I have subdirectory "ReefZones" with all the shapefile information
+head(j)    #same as st .. but with REEF_ZONE added at the end
+p<-get_RZ(st[st$ISLAND=="Palmyra",], readOGR("./ReefZones/pal_reefzones.shp"))   #I have subdirectory "ReefZones" with all the shapefile information
+head(p)    #same as st .. but with REEF_ZONE added at the end
+
+
+rz<-rbind(k, j, p)
+summary(rz)
+
+rz[is.na(rz$REEF_ZONE) & rz$ISLAND=="Johnston",]$REEF_ZONE<-"Protected Slope"   #have got this from some checking of the specific point - against other known segments
+
+rz[rz$REEF_ZONE=="Unknown",]  # Bunch of deeper outer Palmyra - that should be Forereef I assume
+rz[rz$REEF_ZONE=="Unknown",]$REEF_ZONE<-"Forereef"
+
+save(rz, file="PRIA Tow RZ.RData")  #USe this file to get the correct ReefZone information
+
+
