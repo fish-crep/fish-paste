7099d0eb26c593baa5f3d149ad3a32269c37b6df
kaylynmccoy <kaylyn.mccoy@noaa.gov>
2017-10-26 14:25:22 -1000

    Get PRIA TowRZ
    Added FFS reef zone to PRIA reef zones

--- munge_PRIMNM/Get PRIA TowRZ.R
+++ munge_PRIMNM/Get PRIA TowRZ.R
@@ -8,11 +8,18 @@ library(rgdal)
 library(rgeos)
 library(maptools)
 
-st<-read.csv("tmpPRIAtow.csv")    		#I created this already .. its a list of all the segments  
+# load PRIA segment info
+stp<-read.csv("D:/CRED/PRIMNM_REPORT/data/tmpPRIAtow.csv")    		#I created this already .. its a list of all the  PRIA segments  
+
+# load FFS segment info
+ffs<-read.csv("D:/CRED/Towboard/data/tmpFFStow.csv")
+# merge to one file
+st<-rbind(ffs,stp)
+
 st$LONGITUDE<-st$CENTROIDLON			#creating LATITUDE and LONGITUDE fields .. as that is what the code below expects
 st$LATITUDE<-st$CENTROIDLAT
 head(st)
-st$x<-NULL
+st$X<-NULL
 
 source("/Users/ivor.williams/Documents/CRED/Fish Team/Git/fish-paste/lib/fish_team_functions.R")
 
@@ -52,22 +59,27 @@ get_RZ<-function(x, shp){
 } # end get_RZ
 
 
-k<-get_RZ(st[st$ISLAND=="Kingman",], readOGR("./ReefZones/kin_reefzones.shp"))   #I have subdirectory "ReefZones" with all the shapefile information
+k<-get_RZ(st[st$ISLAND=="Kingman",], readOGR("D:/CRED/fish_team_R/fish-paste/data/sector_mpa_shape_files/kin_reefzones.shp"))   #I have subdirectory "ReefZones" with all the shapefile information
 head(k)    #same as st .. but with REEF_ZONE added at the end
-j<-get_RZ(st[st$ISLAND=="Johnston",], readOGR("./ReefZones/joh_reefzones.shp"))   #I have subdirectory "ReefZones" with all the shapefile information
+j<-get_RZ(st[st$ISLAND=="Johnston",], readOGR("D:/CRED/fish_team_R/fish-paste/data/sector_mpa_shape_files/joh_reefzones.shp"))   #I have subdirectory "ReefZones" with all the shapefile information
 head(j)    #same as st .. but with REEF_ZONE added at the end
-p<-get_RZ(st[st$ISLAND=="Palmyra",], readOGR("./ReefZones/pal_reefzones.shp"))   #I have subdirectory "ReefZones" with all the shapefile information
+p<-get_RZ(st[st$ISLAND=="Palmyra",], readOGR("D:/CRED/fish_team_R/fish-paste/data/sector_mpa_shape_files/pal_reefzones.shp"))   #I have subdirectory "ReefZones" with all the shapefile information
 head(p)    #same as st .. but with REEF_ZONE added at the end
+f<-get_RZ(st[st$ISLAND=="French Frigate",], readOGR("D:/CRED/fish_team_R/fish-paste/data/sector_mpa_shape_files/ffs_reefzones.shp"))   #I have subdirectory "ReefZones" with all the shapefile information
+head(f)    #same as st .. but with REEF_ZONE added at the end
 
-
-rz<-rbind(k, j, p)
+rz<-rbind(k, j, p,f)
 summary(rz)
 
 rz[is.na(rz$REEF_ZONE) & rz$ISLAND=="Johnston",]$REEF_ZONE<-"Protected Slope"   #have got this from some checking of the specific point - against other known segments
+rz[is.na(rz$REEF_ZONE) & rz$SEGMENTID==20100910101,]$REEF_ZONE<-"Forereef"   #have got this from some checking of the specific point - against other known segments
+# several segments with NA - all tows with no associated data, so remove them
+rz<-rz[complete.cases(rz$REEF_ZONE),]
 
 rz[rz$REEF_ZONE=="Unknown",]  # Bunch of deeper outer Palmyra - that should be Forereef I assume
 rz[rz$REEF_ZONE=="Unknown",]$REEF_ZONE<-"Forereef"
-
+getwd()
 save(rz, file="PRIA Tow RZ.RData")  #USe this file to get the correct ReefZone information
 
+#save(rz, file="D:/CRED/fish_cruise_routine_report/monitoring_report/2017_status_report/data/Data Outputs/tow_diver/PRIA Tow RZ.RData")  #USe this file to get the correct ReefZone information
 
