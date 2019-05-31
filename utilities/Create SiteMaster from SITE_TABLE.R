bb3e046c71a70b894233c1f94b614fa9a043066a
Ivor <ivor.williams@noaa.gov>
2019-05-30 17:06:58 -1000

    Cleaned up SiteMaster with new SITEVISITIDs
    also added utility to create the Site Master table from the Site_SiteVisit table

---
+++ utilities/Create SiteMaster from SITE_TABLE.R
@@ -0,0 +1,102 @@
+rm(list=ls())
+setwd("/Users/ivor.williams/Documents/CRED/Fish Team/Base R/Base Data Files")
+
+library(gdata)             # needed for drop_levels()
+library(reshape)           # reshape library inclues the cast() function used below
+library(RODBC)            # to connect to oracle
+source("/Users/ivor.williams/Documents/CRED/Fish Team/Git/fish-paste/lib/core_functions.R")
+source("/Users/ivor.williams/Documents/CRED/Fish Team/Git/fish-paste/lib/fish_team_functions.R")
+#source("/Users/ivor.williams/Documents/CRED/Fish Team/Git/fish-paste/lib/Islandwide Mean&Variance Functions.R")
+
+
+## CREATE A SURVEY METADATA file
+load("/Users/ivor.williams/Documents/CRED/Fish Team/Git/fish-paste/data/ALL_SITE_SITEVISIT.RData")
+head(df)
+table(df$TYPE)
+df$OLD_SITE<-df$SITE
+df$XX<-1
+
+#sm target REGION + ISLAND + SITE + REEF_ZONE + DEPTH_BIN + ROUNDID + MISSIONID + SITEVISITID + METHOD + OBS_YEAR + DATE_ + HABITAT_CODE + LATITUDE + LONGITUDE + EXCLUDE_FLAG
+df$OBS_YEAR<-as.numeric(substring(df$DATE_, 1, 4))
+df$SITE<-SiteNumLeadingZeros(df$SITE)
+
+table(df$TYPE)
+df[df$TYPE %in% c("ARMS Only", "ARMS"),]$TYPE<-"ARMS"
+df[df$TYPE %in% c("Benthic Only", "Benthic", "Benthic & CAU"),]$TYPE<-"Benthic"
+df[df$TYPE %in% c("CAU Only", "CAU"),]$TYPE<-"CAU"
+df[df$TYPE %in% c("Oceanography Only", "Oceanography", "Oteam Only"),]$TYPE<-"Oceanography"
+df[df$TYPE %in% c("Fish Only", "Fish"),]$TYPE<-"Fish"
+df[df$TYPE %in% c("Benthic & Fish", "Both"),]$TYPE<-"Both"
+df<-droplevels(df)
+table(df$TYPE)
+
+#sv<-cast(df, REGION + ISLAND + SITE + OLD_SITE + REEF_ZONE + DEPTH_BIN + ROUNDID + MISSIONID + SITEVISITID + FISH_SITEVISITID + BENTHIC_SITEVISITID + OBS_YEAR + DATE_ + HABITAT_CODE + LATITUDE_SV + LONGITUDE_SV + LATITUDE_LOV + LONGITUDE_LOV + EXCLUDE_FLAG + CLIMATE_STATION_YN + PERM_SITE + PERM_MARKER + TRANSECT_PHOTOS + PHOTOMOSAIC_YN + MIN_Z_M+ MAX_Z_M + MIN_DEPTH_M + MAX_DEPTH_M ~ TYPE, value="XX", sum)
+#write.csv(sv, file="tmpALLsurvey.csv")
+sv<-cast(df, REGION + ISLAND + SITE + OLD_SITE + REEF_ZONE + DEPTH_BIN + ROUNDID + MISSIONID + SITEVISITID + OBS_YEAR + DATE_ + HABITAT_CODE + LATITUDE_SV + LONGITUDE_SV + LATITUDE_LOV + LONGITUDE_LOV + EXCLUDE_FLAG + CLIMATE_STATION_YN + PERM_SITE + TRANSECT_PHOTOS + PHOTOMOSAIC_YN + MIN_Z_M+ MAX_Z_M + MIN_DEPTH_M + MAX_DEPTH_M ~ TYPE, value="XX", sum)
+#write.csv(sv, file="tmpALLsurvey.csv")
+head(sv)
+save(sv, file="SURVEY METDATA.RData")
+
+
+load("/Users/ivor.williams/Documents/CRED/Fish Team/Git/fish-paste/data/ALL_REA_FISH_RAW.rdata")
+x<-df
+x$SITE<-SiteNumLeadingZeros(x$SITE)
+length(unique(x$SITEVISITID))
+CCRs<-unique(x[x$METHOD=="nSPC-CCR",]$SITE)
+SPCs<-unique(x[x$SITE %in% CCRs & x$METHOD=="nSPC",]$SITE) #SITES WHERE THERE IS BOTH AN SPC AND A SPC-CCR
+SPCs
+CCRsv<-unique(x[x$METHOD=="nSPC-CCR",]$SITEVISITID)
+dCCRsv<-unique(x[x$SITE %in% SPCs & x$METHOD=="nSPC-CCR",]$SITEVISITID)
+
+#x<-subset(x, x$METHOD %in% c("nSPC", "BLT"))
+# ## Update SITE to have three numeric digits (eg OAH-01 becomes OAH-001)
+#x$DUMMY<-"nFISH_COUNTED"
+#f<-cast(x, REGION + ISLAND + SITE + REEF_ZONE + DEPTH_BIN + ROUNDID + MISSIONID + SITEVISITID + METHOD + OBS_YEAR + DATE_ + HABITAT_CODE + LATITUDE + LONGITUDE + EXCLUDE_FLAG ~ DUMMY, value="COUNT", sum)
+#save(f, file="tmp ALl Fish Surveys.RData")
+# CCR<-unique(f[f$METHOD=="nSPC-CCR",]$SITEVISITID)
+# SPC<-f[f$SITEVISITID %in% CCR & f$METHOD=="nSPC",]$SITEVISITID
+# #write.csv(f[f$SITEVISITID %in% SPC,], file="tmpCCR NW Sites.csv")
+# #bCCR<-CCR[!CCR %in% SPC]
+
+#Now generate SITE DESIGN! (using data from existing SITE MASTER)
+sm<-read.csv("/Users/ivor.williams/Documents/CRED/Fish Team/Git/fish-paste/data/SITE MASTER.csv")
+sm$SITE<-SiteNumLeadingZeros(sm$SITE)
+head(sm)
+dim(sv)
+#FIELDS TO PICK UP FROM SITE MASTER
+SM_FIELDS<-c("SEC_NAME", "HUMANS20", "HUMANS200", "TYPE", "ANALYSIS_SCHEME", "ANALYSIS_YEAR", "TUT2012", "OTHER_AREA_GROUPING", "SPECIAL_PROJ_YN", "SPECIAL_PROJ_DESCRIPTION")
+z<-merge(sv, sm[,c("SITEVISITID", SM_FIELDS)], by="SITEVISITID", all.x=T)
+dim(z)
+
+#fiddling the SCP-CCR sites .. those are sites where there is both an SPC and a CCR
+ccr.sv<-z[z$SITE %in% SPCs,]
+ccr.sv$METHOD<-"xx"
+ccr.sv[ccr.sv$SITEVISITID %in% CCRsv,]$METHOD="CCR"
+c<-subset(ccr.sv, ccr.sv$METHOD %in% "CCR")
+s<-subset(ccr.sv, !ccr.sv$METHOD %in% "CCR")
+
+c<-c[,names(c)[!names(c) %in% SM_FIELDS]]
+c<-merge(c, s[,c("SITE", SM_FIELDS)], by="SITE", all.x=T)
+dim(c)
+cs<-rbind(c,s)
+cs$METHOD<-NULL
+
+zx<-z[!z$SITE %in% SPCs,]
+zg<-rbind(zx, cs)
+
+# # write.csv(zg, "MOD SURVEY MASTER.csv")
+zg[zg$SITE=="TUT-02353",]
+
+write.csv(z, "MOD SURVEY MASTER.csv")
+
+#Now generate SURVEY OCEANOGRAPHY!
+o<-read.csv("/Users/ivor.williams/Documents/CRED/Fish Team/Git/fish-paste/data/SITE OCEANOGR.csv")
+head(o)
+dim(o)
+length(unique(o$SITE))
+names(o)
+sv2<-subset(sv, !sv$SITEVISITID %in% dCCRsv)
+z<-merge(sv2[,c("SITE", "OBS_YEAR", "REGION", "ISLAND", "LATITUDE_SV", "LONGITUDE_SV")], o[,c("SITE", "OBS_YEAR", "MEAN_HII_40", "MEAN_HII_40_ADJUSTED_BY_AREA",                  
+"TUR_MEAN", "TUR_SD", "TUR_CLIM_M", "TUR_ANOM_M", "TUR_ANOM_F", "SST_MEAN", "SST_SD", "SST_CLIM_M", "SST_ANOM_M", "SST_ANOM_F", "PAR_MEAN", "PAR_SD", "PAR_CLIM_M", "PAR_ANOM_M", "PAR_ANOM_F", "CHL_MEAN", "CHL_SD", "CHL_CLIM_M", "CHL_ANOM_M", "CHL_ANOM_F", "WAVES_MEAN", "WAVES_SD", "WAVES_CLIM", "WAVES_ANOM" ,"WAVES_AN_1")])
+save(z, file="SURVEY OCEANOGR.RData")
+
