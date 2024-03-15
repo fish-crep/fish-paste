9332a648e934fe35aa2716f811e336942b359e95
thomas-oliver <thomas.oliver@gmail.com>
2024-03-15 13:00:36 -1000

    Sick of the errors, I fixed it a different way

--- lib/Islandwide Mean&Variance Functions.r
+++ lib/Islandwide Mean&Variance Functions.r
@@ -23,8 +23,10 @@ Calc_PerStrata <- function (sample_data, data_cols, pooling_level = c("ISLAND", 
   strata.vars<-stats::aggregate(sample_data[,data_cols],by=sample_data[,pooling_level], var)
   #Modified TAO 2024-03-12 - there appears to be some weird context-dependent behavior here. I'm trying to fix with stats::3-13; 3-14 stats didn't work, back to SITEVISITID
   #3-15 back to X working better. Sooo weird.
-  N<-stats::aggregate(sample_data[,"SITEVISITID"],by=sample_data[,pooling_level], length)$x
+  #N<-stats::aggregate(sample_data[,"SITEVISITID"],by=sample_data[,pooling_level], length)$x
   #N<-stats::aggregate(sample_data[,"SITEVISITID"],by=sample_data[,pooling_level], length)$SITEVISITID
+  Ndf<-stats::aggregate(sample_data[,"SITEVISITID"],by=sample_data[,pooling_level], length)
+  N<-Ndf[,ncol(Ndf)]
   strata.means$N<-strata.vars$N<-N
   strata.se<-strata.vars
   strata.se[,data_cols]<-sqrt(strata.vars[,data_cols])/sqrt(N)
@@ -457,9 +459,11 @@ tmp_length<-function(x){length(x[!is.na(x)])}
   s.vars<-stats::aggregate(sample_data[,data_cols],by=sample_data[,pooling_level], var, na.rm=T)
   s.N<-stats::aggregate(sample_data[,data_cols],by=sample_data[,pooling_level], tmp_length)
   #Modified TAO 2024-03-12 - there appears to be some weird context-dependent behavior here. I'm trying to fix with stats::3-13; 3-14 stats didn't work, back to SITEVISITID
-  #3-15 back to X working better. Sooo weird.
-  N<-stats::aggregate(sample_data[,"SITEVISITID"],by=sample_data[,pooling_level], length)$x
+  #3-15 back to X working better. Sooo weird. Fixing it another way.
+  #N<-stats::aggregate(sample_data[,"SITEVISITID"],by=sample_data[,pooling_level], length)$x
   #N<-stats::aggregate(sample_data[,"SITEVISITID"],by=sample_data[,pooling_level], length)$SITEVISITID
+  Ndf<-stats::aggregate(sample_data[,"SITEVISITID"],by=sample_data[,pooling_level], length)
+  N<-Ndf[,ncol(Ndf)]
   s.means$N<-s.vars$N<-s.N$N<-N
   s.se<-s.vars
   s.se[,data_cols]<-sqrt(s.vars[,data_cols])/sqrt(s.N[,data_cols])
